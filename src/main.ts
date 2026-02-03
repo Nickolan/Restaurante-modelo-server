1336060c5a16b6d8e396d571b50e4d84c25ef283
Nikoguop <nicolas.navarrete@goup.cloud>
2026-02-03 16:44:04 -0300

    cors frontend

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // La URL de tu frontend
+    origin: 'https://restaurante-modelo-nu.vercel.app', // La URL de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
